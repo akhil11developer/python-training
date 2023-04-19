916822959ea2734168c193f3944ad6558f2c6ab6
akhil11developer <130955394+akhil11developer@users.noreply.github.com>
2023-04-19 10:13:43 +0530

    updated

--- Program/sum loop.py
+++
@@ -1,5 +0,0 @@
-sum = 0    
-for i in range(1,6):   #by default loop start from 1 to 4
-    num = int(input("ENTER NUMBER:"))
-    sum += num
-print("ans =",sum)
--- Program/typecasting.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("enter number:"))
-num2 = int(input("enter number:"))
-
-ans = num1+num2
-
-print(ans)
-
-#typecasting :one datatype to another datatype convert its  called typecasting . 
