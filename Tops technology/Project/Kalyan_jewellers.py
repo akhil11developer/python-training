df2c1e970d97e230c6eba801fa933ae9cd071e2d
akhil11developer <130955394+akhil11developer@users.noreply.github.com>
2023-04-25 21:13:21 +0530

    updated

--- Tops technology/Project/Kalyan_jewellers.py
+++ Tops technology/Project/Kalyan_jewellers.py
@@ -1,19 +1,19 @@
 ##project : KALYAN JEWELLERS
 
-print("kalyan jewellers")
+
+print("------------------KALYAN jewellers----------------------------")
 
 name = input("Enter your name:")
 gender = input("Ente your Gender:")
 age = int(input("Enter your Age:"))
 
+print("----------------------------------------------")
+
 product = input("Enter produt name:")
 product_gram = int(input("Enter Product Gram:"))
+print("Current gold price (1 gram) : 5752 ")
 
-
-"""print("enter name",name)
-print("enter name",gender)
-print("enter name",age)"""
-
+print("----------------------------------------------")
 
 
 goldprice = 5752
@@ -27,7 +27,7 @@ print("Total making charges:",total_making_charges)
 totalamount = goldprice1 + total_making_charges
 print("total amount:",totalamount)
 
-
+print("---------------------------------------------")
 
 
 if gender == "male" and age >65:
@@ -37,6 +37,7 @@ if gender == "male" and age >65:
         print("discount amount 1 : ",disc1)
         totalnet = (totalamount-disc1)
         print("totalnet amount",totalnet)
+        print("--------------------------------------------")
         
     
     elif totalamount >300000 and totalamount <500000:
@@ -45,6 +46,7 @@ if gender == "male" and age >65:
         print("discount amount 2 : ",disc2)
         totalnet = (totalamount-disc2)
         print("totalnet amount",totalnet)
+        print("--------------------------------------------")
         
         
     elif totalamount >300000 and totalamount <500000:
@@ -53,7 +55,7 @@ if gender == "male" and age >65:
         print("discount amount 3 : ",disc3)
         totalnet = (totalamount-disc3)
         print("totalnet amount",totalnet)
-        
+        print("--------------------------------------------")
         
 elif  gender == "male" and age <65:
     if totalamount > 200000 and totalamount < 300000:
@@ -62,7 +64,7 @@ elif  gender == "male" and age <65:
         print("discount_amount 1 : ",discount_amount1)
         totalnet = (totalamount-discount_amount1)
         print("totalnet amount",totalnet)
-        
+        print("--------------------------------------------")
             
     elif totalamount > 300000 and totalamount < 500000:
         discount_amount2 = (totalamount*20)/100
@@ -70,7 +72,7 @@ elif  gender == "male" and age <65:
         print("discount_amount 2 : ",discount_amount2)
         totalnet = (totalamount-discount_amount2)
         print("totalnet amount",totalnet) 
-        
+        print("--------------------------------------------")
            
     elif totalamount > 500000 :
         discount_amount3 = (totalamount*25)/100
@@ -78,7 +80,7 @@ elif  gender == "male" and age <65:
         print("discount_amount 3 : ",discount_amount3)
         totalnet = (totalamount-discount_amount3)
         print("totalnet amount",totalnet)
-        
+        print("--------------------------------------------")
         
 else:
     if gender =="female" and age >65:
@@ -88,7 +90,7 @@ else:
             print("discount_amount 1:",disc1)
             totalnet = (totalamount-disc1)
             print("totalnet amount",totalnet)
-            
+            print("--------------------------------------------")
             
         elif totalamount >300000 and totalamount <500000:
             disc2 = (totalamount*35)/100
@@ -96,7 +98,7 @@ else:
             print("discount amount 2 : ",disc2)
             totalnet = (totalamount-disc2)
             print("totalnet amount",totalnet)
-            
+            print("--------------------------------------------")
         
         elif totalamount <500000:
             disc3 = (totalamount*40)/100
@@ -104,7 +106,7 @@ else:
             print("discount amount 3 : ",disc3)
             totalnet = (totalamount-disc3)
             print("totalnet amount",totalnet)
-            
+            print("--------------------------------------------")
             
     elif gender =="female" and age <65:
         if totalamount >200000 and totalamount <300000:
@@ -113,6 +115,7 @@ else:
             print("discount_amount 1:",disc1)
             totalnet = (totalamount-disc1)
             print("totalnet amount",totalnet)
+            print("--------------------------------------------")
             
         elif totalamount >300000 and totalamount <500000:
             disc2 = (totalamount*25)/100
@@ -120,14 +123,15 @@ else:
             print("discount amount 2 : ",disc2)
             totalnet = (totalamount-disc2)
             print("totalnet amount",totalnet)
-        
+            print("--------------------------------------------")
+            
         elif totalamount <500000:
             disc3 = (totalamount*30)/100
             print("discount : 30%")
             print("discount amount 3 : ",disc3)
             totalnet = (totalamount-disc3)
             print("totalnet amount",totalnet)
-    
+            print("--------------------------------------------")    
       
         
     
